a8adf7baf644e485b6ba4d869a67ec5ae8961749
Christian Müller <hello@christianmueller.dev>
2020-04-04 11:41:25 +0200

    optimize webpack build

--- packages/jarveybot-vue/src/demo.tsx
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import plugin from '@vue/composition-api';
-import DemoApp from './demo-app';
-
-Vue.use(plugin);
-
-new Vue({
-  render: (h) => h(DemoApp),
-}).$mount('#jb-demo');
--- packages/jarveybot-vue/webpack.config.js
+++ packages/jarveybot-vue/webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.tsx',
-    demo: './src/demo.tsx',
-  },
+  entry: { index: './src/index.tsx' },
+  mode: 'production',
+  devtool: 'source-map',
   resolve: {
     extensions: [
       '.ts',
@@ -19,18 +17,19 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/dist'),
     filename: '[name].js',
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: ['babel-loader', 'ts-loader'],
-      },/*
+      },
       {
         enforce: 'pre',
         test: /\.js$/,
-        loader: 'source-map-loader'
-      },*/
+        loader: 'source-map-loader',
+      },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
@@ -47,9 +46,6 @@ module.exports = {
     new FriendlyErrorsWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'style.css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
     }),
   ],
 };
